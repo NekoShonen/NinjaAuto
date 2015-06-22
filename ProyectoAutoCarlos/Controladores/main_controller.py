d2d82405271fe043e15afbe7611e90fa09034de5
Alumno <alumno@huayra.(none)>
2015-06-22 14:01:30 -0300

    segunda version

--- ProyectoAutoCarlos/Controladores/main_controller.py
+++ ProyectoAutoCarlos/Controladores/main_controller.py
@@ -3,7 +3,27 @@ import sys
 sys.path.append('../Modelos')
 from auto import *
 
-class MainController():
+class MainControlador():
 
-    def __init__(self):
+    def __init__(self, una_ventana):
         self.auto = Auto()
+        self.ventana = una_ventana
+
+    def handler_subir_persona(self):
+        self.auto.subir_persona()
+        self.actualizar_label()
+        #print self.auto.cantidad_personas
+
+    def handler_bajar_persona(self):
+        self.auto.bajar_persona()
+        self.actualizar_label()
+        #print self.auto.cantidad_personas
+
+    def handler_bajar_persona5(self):
+        i = 1
+        while i <= 5:
+            i += 1
+            self.handler_bajar_persona()
+
+    def actualizar_label(self):
+        self.ventana.label.setText(str(self.auto.cantidad_personas))
--- ProyectoAutoCarlos/Vistas/main_window.py
+++ ProyectoAutoCarlos/Vistas/main_window.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
 import sys
 from PyQt4 import QtGui
-sys.path.append ('../Controladores')
+sys.path.append('../Controladores')
 from main_controller import *
 
 class MainWindow (QtGui.QWidget):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.controlador = MainControlador()
+        self.controlador = MainControlador(self)
         self.init_ui()
 
     def init_ui(self):
-        label = QtGui.QLabel('Cantidad de Personas')
+        self.label = QtGui.QLabel('Cantidad de Personas')
         h_layout = QtGui.QHBoxLayout()
-        h_layout.addWidget(label)
+        h_layout.addWidget(self.label)
         button_subir = QtGui.QPushButton('Subir Persona')
         button_bajar = QtGui.QPushButton('Bajar Persona')
+        button_bajar5 = QtGui.QPushButton('bajar personas 5')
         h_layout.addWidget(button_subir)
         h_layout.addWidget(button_bajar)
-        h_layout.addWidget(label)
+        h_layout.addWidget(button_bajar5)
 
         button_subir.clicked.connect(self.controlador.handler_subir_persona)
-
+        button_bajar.clicked.connect(self.controlador.handler_bajar_persona)
+        button_bajar5.clicked.connect(self.controlador.handler_bajar_persona5)
         self.setLayout(h_layout)
         self.setWindowTitle('Proyecto del Auto')
-        self.Geometry(200,200,200,200)
+        self.setGeometry(200, 200, 200, 200)
         self.show()
 
 app = QtGui.QApplication(sys.argv)
